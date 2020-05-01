17a91c2961c5cb0de976cb9305dd6db0c9a5bb29
Zhenek <54858818+IIPEKOLICT@users.noreply.github.com>
2020-05-01 17:17:09 +0300

    0.6.1

--- kernel/js/trash.js
+++ kernel/js/trash.js
@@ -1,3 +1,95 @@
+/*
+
+let page = !localStorage.getItem('.page');
+let themeButton = !localStorage.getItem('.theme-button');
+themeButton.onclick = function() {
+  page.classList.toggle('dark');
+  page.classList.toggle('light');
+};
+
+// Изменение цвета акцента
+
+$(function() {
+  if(!localStorage.getItem('theme_akcent')) {
+    $('#theme_akcent').attr('href', 'kernel/css/akc/akc1.css');
+  } else {
+    $('#theme_akcent').attr('href', 'kernel/css/akc/akc' + localStorage.getItem('theme_akcent') + '.css');
+  };
+});
+function change_akcent(a) {
+  if(a == 1) {
+    $('#theme_akcent').attr('href', 'kernel/css/akc/akc1.css');
+    localStorage.removeItem('theme_akcent');
+  } else {
+    $('#theme_akcent').attr('href', 'kernel/css/akc/akc' + a + '.css');
+    localStorage.setItem('theme_akcent', a);
+  };
+};
+
+var element = document.querySelector('.accent-color');
+var input = document.querySelector('.input');
+
+input.addEventListener('change', function(){
+  element.style.setProperty('--accent-color', input.value);
+})
+}
+
+function changeAccent(a) {
+    $(':root').changeCss('--accent-color', function(index,a){return a;});
+  localStorage.setItem('changeAccent', a);
+};
+
+if(localStorage.getItem('accentColor')) {
+  var checkedAccentColor = localStorage.getItem('accentColor');
+  document.documentElement.style.setProperty('--accent-color', checkedAccentColor);
+  alert(localStorage.getItem('accentColor'));
+}
+
+alert(localStorage.getItem('accentColor'));
+
+$(function() {
+  if(!localStorage.getItem('accentColor')) {
+    a = !localStorage.getItemValue('accentColor');
+    $(':root').css('--accent-color', a);
+  };
+});
+
+var test = document.getElementsByName('test');
+for (var i = 0; i < test.length; i++) {
+  test[i].onclick = function() {
+    localStorage.setItem('testCheckedItem', this.id);
+  }
+}
+
+// Функции-оптимизаторы
+
+function setRootColor(element, color) {
+  document.documentElement.style.setProperty('"' + element + '"', color);
+}
+
+function test1(key,parametr,default) {
+  if(localStorage.getItem("'" + key + "'")) { // Если в ЛХ есть ключ цвета акцента
+    document.documentElement.style.setProperty("'" + parametr + "'", key); // Установить цвет
+  } else {
+    localStorage.setItem("'" + key + "'", "'" + default + "'"); // Дефолтный ключ, если сохраненного не было
+  }
+}
+
+// Функции-оптимизаторы
+
+function checkLSKey(key,parametr,default) {
+  if(localStorage.getItem("'" + key + "'")) { // Если в ЛХ есть ключ цвета акцента
+    var keyValue = localStorage.getItem("'" + key + "'");
+    document.documentElement.style.setProperty("'" + key + "'", keyValue); // Установить цвет
+  } else {
+    localStorage.setItem("'" + key + "'", "'" + default + "'"); // Дефолтный ключ, если сохраненного не было
+  }
+}
+
+*/
+
+/*
+
 // Переменные
 
 var coloredLabels = document.querySelectorAll('.label_custom-colored'); // Все крашенные value лейблы
@@ -59,6 +151,13 @@ document.addEventListener("DOMContentLoaded", () => { // Событие загу
     localStorage.setItem('mainBorderColor', '#212121'); // Дефолтный ключ, если сохраненного не было
     document.documentElement.style.setProperty('--main_border-color', '#212121');
   }
+  if(localStorage.getItem('headerBackButton')) { // Если в ЛХ есть ключ кнопки назад из хедера
+    var headerBackButton = localStorage.getItem('headerBackButton');
+    document.documentElement.style.setProperty('--header-back_button', headerBackButton); // Установить кнопку
+  } else {
+    localStorage.setItem('headerBackButton', 'url(../icons/back-white.png)'); // Дефолтный ключ, если сохраненного не было
+    document.documentElement.style.setProperty('--header-back_button', 'url(../icons/back-white.png)');
+  }
 
   if(localStorage.getItem('checkedAccentNo')) { // Если в ЛХ есть ключ номера выбранного акц-инпута
     var checkedAccentNo = localStorage.getItem('checkedAccentNo');
@@ -114,14 +213,75 @@ function changeColorScheme(csp1,csp2,csp3,csp4,csp5,csp6,csp7) {
   document.documentElement.style.setProperty('--main_text-color', csp4);
   document.documentElement.style.setProperty('--secondary_text-color', csp5);
   document.documentElement.style.setProperty('--main_border-color', csp6);
+  document.documentElement.style.setProperty('--header-back_button', csp7);
   localStorage.setItem('mainBgColor', csp1);
   localStorage.setItem('secondaryBgColor', csp2);
   localStorage.setItem('hoverBgColor', csp3);
   localStorage.setItem('mainTextColor', csp4);
   localStorage.setItem('secondaryTextColor', csp5);
   localStorage.setItem('mainBorderColor', csp6);
+  localStorage.setItem('headerBackButton', csp7);
 }
 
+*/
+
 /*
+
+// Изменение цвета акцента
+
+$(function() {
+  if(!localStorage.getItem('theme_akcent')) {
+    $('#theme_akcent').attr('href', 'kernel/css/akc/akc1.css');
+  } else {
+    $('#theme_akcent').attr('href', 'kernel/css/akc/akc' + localStorage.getItem('theme_akcent') + '.css');
+  };
+});
+function change_akcent(a) {
+  if(a == 1) {
+    $('#theme_akcent').attr('href', 'kernel/css/akc/akc1.css');
+    localStorage.removeItem('theme_akcent');
+  } else {
+    $('#theme_akcent').attr('href', 'kernel/css/akc/akc' + a + '.css');
+    localStorage.setItem('theme_akcent', a);
+  };
+};
+
+// Изменение цвета фона
+
+$(function() {
+  if(!localStorage.getItem('theme_background')) {
+    $('#theme_background').attr('href', 'root/css/background/bg1.css');
+  } else {
+    $('#theme_background').attr('href', 'root/css/background/bg' + localStorage.getItem('theme_background') + '.css');
+  };
+});
+function change_background(a) {
+  if(a == 1) {
+    $('#theme_background').attr('href', 'root/css/background/bg1.css');
+    localStorage.removeItem('theme_background');
+  } else {
+    $('#theme_background').attr('href', 'root/css/background/bg' + a + '.css');
+    localStorage.setItem('theme_background', a);
+  };
+};
+
+// Включение/выключение закруглений
+
+$(function() {
+  if(!localStorage.getItem('theme_krug')) {
+    $('#theme_krug').attr('href', 'root/css/krug/kr1.css');
+  } else {
+    $('#theme_krug').attr('href', 'root/css/krug/kr' + localStorage.getItem('theme_krug') + '.css');
+  };
+});
+function change_krug(a) {
+  if(a == 1) {
+    $('#theme_krug').attr('href', 'root/css/krug/kr1.css');
+    localStorage.removeItem('theme_krug');
+  } else {
+    $('#theme_krug').attr('href', 'root/css/krug/kr' + a + '.css');
+    localStorage.setItem('theme_krug', a);
+  };
+};
 
 */
